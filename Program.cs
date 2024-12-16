5fbe9168050000e2f3e3bedd2af3895ee43826bd
ClimaCare <imetodi90@gmail.com>
2024-12-16 15:50:32 +0200

    Task1 added

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Choose a task to execute (1-5):");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Task1.Execute();
+                    break;
+                //case "2":
+                //    Task2MaxSequenceOfEqualElements.Execute();
+                //    break;
+                //case "3":
+                //    Task3Train.Execute();
+                //    break;
+                //case "4":
+                //    Task4CardsGame.Execute();
+                //    break;
+                //case "5":
+                //    Task5CoauntCharsInString.Execute();
+                //    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Task1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using ConsoleApp9;
+
+namespace ConsoleApp9
+{
+    class Task1
+    {
+        abstract class Animal
+        {
+            public string Name { get; set; }
+
+            public Animal(string name)
+            {
+                Name = name;
+            }
+
+            public abstract void MakeSound();
+
+            public void Eat()
+            {
+                Console.WriteLine($"{Name} is eating.");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public Dog(string name) : base(name) { }
+
+            public override void MakeSound()
+            {
+                Console.WriteLine($"{Name} says: Bark!");
+            }
+        }
+
+        class Cat : Animal
+        {
+            public Cat(string name) : base(name) { }
+
+            public override void MakeSound()
+            {
+                Console.WriteLine($"{Name} says: Meow!");
+            }
+        }
+
+        public static void Execute()
+        {
+            List<Animal> animals = new List<Animal>
+        {
+            new Dog("Rocky"),
+            new Cat("Snowball"),
+            new Dog("Hector"),
+            new Cat("Bella")
+        };
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeSound();
+                animal.Eat();
+            }
+        }
+    }
+}
