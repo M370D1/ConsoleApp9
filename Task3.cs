0b1162806a61e147c05050ec3931528444e5b810
ClimaCare <imetodi90@gmail.com>
2024-12-17 13:06:31 +0200

    Task3 correction

--- Task3.cs
+++ Task3.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp9
+﻿namespace ConsoleApp9
 {
-    internal class Task3
+    class Task3
     {
+        public abstract class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Product: {Name}, Price: {Price:C}");
+            }
+
+            public abstract decimal GetDiscountedPrice();
+        }
+
+        public class Electronics : Product
+        {
+            public override decimal GetDiscountedPrice()
+            {
+                return Price * 0.90M;
+            }
+        }
+
+        public class Clothing : Product
+        {
+            public override decimal GetDiscountedPrice()
+            {
+                return Price * 0.80M;
+            }
+        }
+
+        public static void Execute()
+        {
+            var laptop = new Electronics { Name = "Laptop", Price = 1500M };
+            var tshirt = new Clothing { Name = "T-Shirt", Price = 50M };
+
+            List<Product> products = new List<Product> { laptop, tshirt };
+
+            foreach (var product in products)
+            {
+                product.DisplayInfo();
+                Console.WriteLine($"Discounted Price: {product.GetDiscountedPrice():C}\n");
+            }
+        }
+
     }
 }
