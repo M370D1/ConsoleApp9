ff9231bad35abbb082448b411f0466ae6c96a896
ClimaCare <imetodi90@gmail.com>
2024-12-17 11:47:16 +0200

    Task2 added

---
+++ Task2.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp9
+{
+    class Task2
+    {
+        public interface IGradeCalculator
+        {
+            double CalculateGrade();
+        }
+
+        public class Assignment : IGradeCalculator
+        {
+            public double MaxScore { get; set; }
+            public double ScoreAchieved { get; set; }
+
+            public double CalculateGrade()
+            {
+                return (ScoreAchieved / MaxScore) * 100;
+            }
+        }
+
+        public class Exam : IGradeCalculator
+        {
+            public double MaxMarks { get; set; }
+            public double MarksObtained { get; set; }
+
+            public double CalculateGrade()
+            {
+                return (MarksObtained / MaxMarks) * 100;
+            }
+        }
+
+        public static void Execute()
+        {
+            Assignment assignment = new Assignment { MaxScore = 100, ScoreAchieved = 85 };
+            Exam exam = new Exam { MaxMarks = 100, MarksObtained = 92 };
+
+            List<IGradeCalculator> gradeCalculators = new List<IGradeCalculator> { assignment, exam };
+
+            foreach (var calculator in gradeCalculators)
+            {
+                Console.WriteLine($"{calculator.GetType().Name} Grade: {calculator.CalculateGrade():F2}%");
+            }
+        }
+
+    }
+}
